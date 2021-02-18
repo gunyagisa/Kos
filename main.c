396c063e1a6a631b5bbfe09745d7bcf832781c99
gunyagisa <gunyagisa@gmail.com>
2021-02-18 22:02:13 +0900

    EFI_FILE_PROTOCOLの対応
    BootServiceのLocate_Protocolを使ってEFI_SIMPLE_FILE_SYSTEM_PROTOCOLを利用できるようにする。
    EFI_SIMPLE_FILE_SYSTEM_PROTOCOLのOpenVolume関数を使ってEFI_FILE_PROTOCOLを取得し、
    Open関数でroot直下のkernel.binを読み込めるようになった。

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdint.h>
 
+#define NULL 0
+#define EFI_SUCCESS 0
+
 typedef uint64_t UINTN;
 typedef void* EFI_HANDLE;
+typedef uint64_t EFI_STATUS;
 
-typedef void EFI_STATUS;
+typedef struct {
+  uint32_t guid1;
+  uint16_t guid2;
+  uint16_t guid3;
+  uint8_t  guid4[8];
+} EFI_GUID;
 
 typedef struct {
   uint64_t sig;
@@ -29,6 +38,8 @@ typedef struct {
                                 UINTN *MapKey, UINTN *DescriptroSize, uint32_t *DescriptroVersion);
   uint64_t      buf2[21];
   uint64_t      (*ExitBootServices)(EFI_HANDLE ImageHandle, UINTN MapKey);
+  uint64_t      buf3[10];
+  uint64_t      (*LocateProtocol)(EFI_GUID *Protocol, void *Registration, void **Interface);
 } EFI_BOOT_SERVICES;
 
 typedef struct {
@@ -64,17 +75,69 @@ typedef struct {
   EFI_CONFIGURATION_TABLE               *ConfigTable;
 } EFI_SYSTEM_TABLE;
 
+// EFI_SIMPLE_FILE_SYSTEM_PROTOCOL
+//
+#define EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID \
+ {0x0964e5b22,0x6459,0x11d2,\
+ {0x8e,0x39,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
+struct EFI_FILE_PROTOCOL;
+
+typedef struct EFI_SIMPLE_FILE_SYSTEM_PROTOCOL {
+  uint64_t Rev;
+  uint64_t (*OpenVolume)(struct EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *This, struct EFI_FILE_PROTOCOL **Root);
+} EFI_SIMPLE_FILE_SYSTEM_PROTOCOL;
+
+// EFI_FILE_PROTOCOL
+// structure
+typedef struct EFI_FILE_PROTOCOL {
+  uint64_t rev;
+  uint64_t (*Open)(struct EFI_FILE_PROTOCOL *This, 
+                   struct EFI_FILE_PROTOCOL **NewHandle, 
+                   uint16_t *FileName, 
+                   uint64_t OpenMode, uint64_t Attributes);
+  uint64_t buf[13];
+} EFI_FILE_PROTOCOL;
+
+// Open Modes
+#define EFI_FILE_MODE_READ      0x0000000000000001
+#define EFI_FILE_MODE_WRITE     0x0000000000000002
+#define EFI_FILE_MODE_CREATE    0x8000000000000000
+
+
 void ExitBootServices ( EFI_HANDLE ImageHandle, UINTN MapKey);
 
 
 EFI_STATUS UefiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
-  SystemTable->ConOut->clearScreen(SystemTable->ConOut);
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello UEFI\n");
-
   UINTN mapsize, mapkey, descriptorsize;
   EFI_MEMORY_DESCRIPTOR map;
   uint32_t desc_ver;
+  unsigned long long status;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *SFSP;
+  EFI_FILE_PROTOCOL *root;
+  EFI_FILE_PROTOCOL *kernel;
+
+
+  SystemTable->ConOut->clearScreen(SystemTable->ConOut);
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello UEFI\n\r");
+
+  //
+  EFI_GUID sfsp_guid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+  SystemTable->BootServices->LocateProtocol(&sfsp_guid, NULL, (void **)&SFSP);
+
+  status = SFSP->OpenVolume(SFSP, &root);
+  if (status == EFI_SUCCESS) {
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Success OpenVolume\n\r");
+  }
+
+
+  // load the kernel file
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Start Loading Kos Kernel.\n\r");
+
+  status = root->Open(root, &kernel, L"kernel.bin", EFI_FILE_MODE_READ, 0);
+  if (status == EFI_SUCCESS) {
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Success root->Open\n\r");
+  }
 
   SystemTable->BootServices->GetMemoryMap(&mapsize, &map, &mapkey, &descriptorsize, &desc_ver);
   SystemTable->BootServices->ExitBootServices(ImageHandle, mapkey);
