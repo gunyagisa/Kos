32f5503c235bedb462f81146461732e8fe0bbac2
gunyagisa <gunyagisa@gmail.com>
2021-02-21 02:23:33 +0900

    画面描画用の関数を追加

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,3 +1,5 @@
+#include "font.h"
+
 struct framebuffer {
   unsigned long long base;
   unsigned long long size;
@@ -5,6 +7,9 @@ struct framebuffer {
   unsigned long long y_size;
   unsigned int       pps;
 } fb;
+
+void clear_screen();
+void draw_chr(unsigned int x, unsigned int y, unsigned char r, unsigned char g, unsigned char b, unsigned char c);
 
 int kernel_init(struct framebuffer *_fb)
 {
@@ -14,13 +19,54 @@ int kernel_init(struct framebuffer *_fb)
   fb.y_size = _fb->y_size;
   fb.pps = _fb->pps;
 
-  unsigned int pixel = 0xffffffff;
+  clear_screen();
+
+  draw_chr(0, 0, 0xff, 0xff, 0xff, 'H');
+  draw_chr(8, 0, 0xff, 0xff, 0xff, 'E');
+  draw_chr(16, 0, 0xff, 0xff, 0xff, 'L');
+  draw_chr(24, 0, 0xff, 0xff, 0xff, 'L');
+  draw_chr(32, 0, 0xff, 0xff, 0xff, 'O');
+
+  for (;;) ;
+}
+
+void clear_screen()
+{
   unsigned int *vram = (unsigned int *)fb.base;
   for (int y = 0; y < fb.y_size; ++y) {
     for (int x = 0; x < fb.x_size; ++x) {
-      vram[x + y * fb.x_size] = pixel;
+      vram[x + y * fb.x_size] = 0xff000000;
+    }
+  }
+}
+
+void draw_pixel (unsigned x, unsigned int y, unsigned char r, unsigned char g, unsigned char b)
+{
+  unsigned int *vram = (unsigned int *) fb.base;
+  struct PIXEL {
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+    unsigned char yokuwakarann;
+  } pixel;
+  pixel.red = r;
+  pixel.green = g;
+  pixel.blue = b;
+  pixel.yokuwakarann = 0xff;
+
+  unsigned int *color = (unsigned int *)&pixel; 
+
+  vram[x + y * fb.x_size] = *color;
+}
+
+void draw_chr(unsigned int x, unsigned int y, unsigned char r, unsigned char g, unsigned char b, unsigned char c)
+{
+  for (int i = 0; i < 16; ++i) {
+    for (int j = 0; j < 8; ++j) {
+      if ((font[c * 16 + i] & (0x01 << (7 - j))) != 0) {
+        draw_pixel(x + j, y + i, r, g, b);
+      }
     }
   }
 
-  for (;;) ;
 }
